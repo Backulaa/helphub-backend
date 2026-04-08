760298aa8c8585cfa679da06e9e1dc0377fd52d5
Backulmn <newomgs222@gmail.com>
2026-04-08 20:01:13 +0700

    chore(config): setup application configuration, dependencies and timezone

--- src/main/java/com/helphub/backend/HelphubBackendApplication.java
+++ src/main/java/com/helphub/backend/HelphubBackendApplication.java
@@ -1,5 +1,9 @@
 package com.helphub.backend;
 
+import com.helphub.backend.common.util.DateTimeUtils;
+
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelphubBackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(DateTimeUtils.VN_ZONE));
         SpringApplication.run(HelphubBackendApplication.class, args);
     }
-
 }
